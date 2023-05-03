532c6976b80e6a1368448cb93c935de36b17054e
zala02 <zu0476@student.uni-lj.si>
2023-05-03 19:50:31 +0200

    gravitacija.java

---
+++ src/gravitacija.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Gravitacija {
+    public static void main(String[] args) {
+
+        System.out.println("OIS je zakon!");
+
+        /* Scanner scanner = new Scanner(System.in);
+        int visina = scanner.nextInt();
+
+        int C = 6.674 x 10 ^ -11;
+        int r = 6.371 x 10 ^ 6;
+        int a;
+
+        a = (C x M) / (r + visina)^2;
+
+        System.out.println(a); */
+    }
+}
